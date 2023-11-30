6c4b292c8d4a668fafcffec6ce9f8ae7c61f79e9
JhuderPalomino <jhuder.palomino.27@unsch.edu.pe>
2023-11-29 21:40:12 -0500

    feat: add style

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -1,12 +1,12 @@
 <template>
-  <div>
+  <div class="login-container">
     <h2>Login</h2>
-    <form @submit.prevent="login">
+    <form @submit.prevent="login" class="login-form">
       <label>Email:</label>
-      <input v-model="email" type="email" required>
+      <input v-model="email" type="email" required class="input-field">
       <label>Contraseña:</label>
-      <input v-model="password" type="password" required>
-      <button type="submit">Iniciar sesión</button>
+      <input v-model="password" type="password" required class="input-field">
+      <button type="submit" class="login-button">Iniciar sesión</button>
     </form>
   </div>
 </template>
@@ -31,8 +31,42 @@ export default {
         // Puedes redirigir a otra página después del inicio de sesión exitoso.
       } catch (error) {
         console.error('Error de inicio de sesión:', error.message);
+
       }
     },
   },
 };
 </script>
+<style scoped>
+.login-container {
+  max-width: 400px;
+  margin: 0 auto;
+  padding: 20px;
+  border: 1px solid #ccc;
+  border-radius: 5px;
+  box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
+}
+
+.login-form {
+  display: flex;
+  flex-direction: column;
+}
+
+.input-field {
+  margin-bottom: 10px;
+  padding: 8px;
+}
+
+.login-button {
+  background-color: #4caf50;
+  color: white;
+  padding: 10px;
+  border: none;
+  border-radius: 5px;
+  cursor: pointer;
+}
+
+.login-button:hover {
+  background-color: #45a049;
+}
+</style>
