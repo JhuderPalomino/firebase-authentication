2908e8c72388113a8ff2553a5533612265dde569
JhuderPalomino <jhuder.palomino.27@unsch.edu.pe>
2023-11-30 12:40:26 -0500

    refactor: environment variables

--- src/firebase.js
+++ src/firebase.js
@@ -1,13 +1,9 @@
 import { initializeApp } from 'firebase/app';
 import { getAuth } from 'firebase/auth';
+import { FIREBASE_API_KEY } from "@/components/app-constants";
 
 const firebaseConfig = {
-  apiKey: 'TU_API_KEY',
-  authDomain: 'TU_DOMINIO.firebaseapp.com',
-  projectId: 'TU_ID_DE_PROYECTO',
-  storageBucket: 'TU_BUCKET.appspot.com',
-  messagingSenderId: 'TU_ID_DE_MENSAJERÍA',
-  appId: 'TU_APP_ID',
+  apiKey: FIREBASE_API_KEY,
 };
 
 const firebaseApp = initializeApp(firebaseConfig);
